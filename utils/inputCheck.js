4c7d74b89edde8be65c5aedd32dafbb0e1b671a4
sehaj97 <sehajmagan@gmail.com>
2022-01-15 15:30:50 -0500

    Added voter app

--- utils/inputCheck.js
+++ utils/inputCheck.js
@@ -4,7 +4,7 @@
 module.exports = function(obj, ...props) {
   const errors = [];
 
-  props.forEach((prop) => {
+  props.forEach(prop => {
     // if property is blank or doesn't exist, add to errors array
     if (obj[prop] === undefined || obj[prop] === '') {
       errors.push(`No ${prop} specified.`);
@@ -17,20 +17,5 @@ module.exports = function(obj, ...props) {
     };
   }
 
-  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
-  VALUES (?,?,?)`;
-const params = [body.first_name, body.last_name, body.industry_connected];
-
-db.query(sql, params, (err, result) => {
-  if (err) {
-    res.status(400).json({ error: err.message });
-    return;
-  }
-  res.json({
-    message: 'success',
-    data: body
-  });
-});
-  
   return null;
 };
